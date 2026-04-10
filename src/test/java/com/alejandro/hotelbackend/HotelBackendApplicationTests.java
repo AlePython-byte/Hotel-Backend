e59f24b2d32aa98b4ce1fc049e836e809a116c6f
Alejandro <jorge.parrab@campusucc.edu.co>
2026-04-10 14:28:01 -0500

    creacion del backend

---
+++ src/test/java/com/alejandro/hotelbackend/HotelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.hotelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
